b7126e64795fbad8c8e74e8a65182863593dadd3
hazle09 <joohyenim@gmail.com>
2020-05-18 23:07:10 +0900

    주석추가

--- src/algorithm/UniqueChar_1_1.java
+++ src/algorithm/UniqueChar_1_1.java
@@ -4,10 +4,12 @@ package algorithm;
 public class UniqueChar_1_1 {
 	
 	public static boolean uniqueChar(String str) {
+		//아스키코드의 문자 128개만큼의 크기를 가진 배열 생성
 		boolean arraychar[]=new boolean[128];
 		if(str.length()>128) return false;
 		
 		for(int i=0;i<str.length();i++) {
+			//char를 int로 저장함으로써 아스키코드 값으로 저장, 배열의 각 인덱스로 연결됨.
 			int val=str.charAt(i);
 			if(arraychar[val])return false;
 			arraychar[val]=true;
